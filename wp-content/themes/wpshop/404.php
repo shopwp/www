33fe8666afa664b6119cbfa2d99d9a7cf0711fdf
Andrew <arobbins@simpleblend.net>
2018-05-07 14:29:05 -0500

    Forced update

--- wp-content/themes/wpshop/404.php
+++ wp-content/themes/wpshop/404.php
@@ -4,4 +4,4 @@
   <?php _e('Woops, looks like that page doesn\'t exist. If you\'re having trouble finding what you\'re looking for <a href="https://join.slack.com/wpshopify/shared_invite/MTg5OTQxODEwOTM1LTE0OTU5ODY2MTktN2Y1ODk0YzBlNg">join our Slack channel</a> and you\'ll be helped as soon as possible!', 'sage'); ?>
 </div>
 
-<?php get_search_form(); ?>
+<?php get_search_form();
